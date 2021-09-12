2204141c281837e4317253a6eb8fc8a9a463f52e
dhnation <danielhennig91@web.de>
2021-09-12 15:16:47 +0100

    upload bookmark landing page

---
+++ Bookmark Landing Page/README.md
@@ -0,0 +1,49 @@
+# Frontend Mentor - Bookmark landing page solution
+
+This is a solution to the [Bookmark landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/bookmark-landing-page-5d0b588a9edda32581d29158). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Receive an error message when the newsletter form is submitted if:
+  - The input field is empty
+  - The email address is not formatted correctly
+
+### Screenshot
+
+
+### Links
+
+- Solution URL: [Preview Solution](https://your-solution-url.com)
+- Live Site URL: [See Live-Version](https://bookmarklp-by-danielhennig.netlify.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS3
+- SCSS
+- JacaScript
+- Mobile-first workflow
+- Adobe XD
+
+## Author
+
+- Frontend Mentor - [@daniel-hennig](https://www.frontendmentor.io/profile/daniel-hennig)
+- freeCodeCamp - [@daniel-hennig](https://www.freecodecamp.org/daniel-hennig)
---
+++ Bookmark Landing Page/app/script.js
@@ -0,0 +1,157 @@
+'use strict'
+
+// hamburger + overlay
+const hamburger = document.getElementById("hamburger");
+const navBar = document.getElementById("nav-bar");
+const navLogoImg = document.getElementById("nav-logo-img");
+const navBarItem = document.querySelectorAll(".nav_bar_item");
+const body = document.querySelector("body");
+
+let active = false;
+
+hamburger.addEventListener("click", (e) => {
+    if(active) { // close menu
+        e.preventDefault();
+
+        navBar.classList.remove("fade-in");
+        navBar.classList.add("fade-out");
+        
+        hamburger.children[0].classList.remove("first-span-anima");
+        hamburger.children[2].classList.remove("third-span-anima");
+        
+        hamburger.children[0].classList.add("reverse-first-span-anima");
+        hamburger.children[2].classList.add("reverse-third-span-anima");
+        hamburger.children[1].style.opacity = 1;
+
+        navLogoImg.children[0].attributes.fill.nodeValue = 'initial';
+        navLogoImg.children[1].children[0].attributes.fill.nodeValue = "#5267df"
+        navLogoImg.children[1].children[1].attributes.fill.nodeValue = "#fff"
+        
+        navBarItem.forEach(item => {
+            item.style.opacity = "0";
+        });
+        
+        body.style.overflow = "initial";
+
+        active = false;
+    } else { // open menu
+        e.preventDefault();
+
+        if(!navBar.classList.contains("overlay")) {
+            navBar.classList.add("overlay");
+            navBar.classList.add("fade-in");
+        } else {
+            navBar.classList.remove("fade-out");
+            navBar.classList.add("fade-in");
+        }
+
+        if(hamburger.children[0].classList.contains("reverse-first-span-anima") && hamburger.children[2].classList.contains("reverse-third-span-anima")) {
+            hamburger.children[0].classList.remove("reverse-first-span-anima");            
+            hamburger.children[2].classList.remove("reverse-third-span-anima");            
+        }
+
+        hamburger.children[1].style.opacity = 0;
+        hamburger.children[0].classList.add("first-span-anima");
+        hamburger.children[2].classList.add("third-span-anima");
+
+        navLogoImg.children[0].attributes.fill.nodeValue = '#fff';
+        navLogoImg.children[1].children[0].attributes.fill.nodeValue = "#fff"
+        navLogoImg.children[1].children[1].attributes.fill.nodeValue = "#30354F"
+
+        navBarItem.forEach(item => {
+            item.style.opacity = "1";
+        });
+
+        body.style.overflow = "hidden";
+
+        active = true;
+    }
+});
+
+// slider
+const tabList = document.getElementById("tab-list").children;
+const slideContainer = document.querySelectorAll(".slide-container");
+
+for(let i = 0; i < tabList.length; i++) {
+    tabList[i].firstChild.addEventListener(("click"), function(e) {
+
+        e.preventDefault();
+
+        if(tabList[i].firstChild.id === "tab-bookmarking") {
+            slideContainer.forEach(slide => {
+                for(let j = 0; j < tabList.length; j++) {
+                    tabList[j].firstChild.className = "";
+                }
+
+                tabList[i].firstChild.className = "active";
+                slide.style.transform = "translateX(100%)";
+            });
+        }
+
+        if(tabList[i].firstChild.id === "tab-searching") {
+            slideContainer.forEach(slide => {
+                for(let j = 0; j < tabList.length; j++) {
+                    tabList[j].firstChild.className = "";
+                }
+
+                tabList[i].firstChild.className = "active";
+                slide.style.transform = "translateX(0)";
+            });
+        }
+
+        if(tabList[i].firstChild.id === "tab-sharing") {
+            slideContainer.forEach(slide => {
+                for(let j = 0; j < tabList.length; j++) {
+                    tabList[j].firstChild.className = "";
+                }
+
+                tabList[i].firstChild.className = "active";
+                slide.style.transform = "translateX(-100%)";
+            });
+        }
+    });
+}
+
+// accordion
+const accordion = document.getElementsByClassName("accordion-box");
+
+for(let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener(("click"), function() {
+        this.classList.toggle("active");
+
+        // toggle transition
+        if(this.children[1].style.height === "" || this.children[1].style.height === "0px") {
+            this.children[1].style.height = this.children[1].scrollHeight + "px";
+        } else {
+            this.children[1].style.height = 0;
+        }
+    });
+}
+
+// form email-validation
+const contactBtn = document.getElementById("contact-btn");
+
+contactBtn.addEventListener("click", (e) => {
+    const emailInput = document.getElementById("email-input").value;
+    const exclamationMark = document.querySelector(".exclamation-mark");
+    const errorMessage = document.querySelector(".error-message");
+    
+    e.preventDefault();
+
+    function validateEmail(email) {
+        const regex = /^(([^<>()äüöü\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        // const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;    
+
+        return regex.test(email);
+    }    
+
+    if(validateEmail(emailInput) === true) {
+        if(exclamationMark.classList.contains("error") || errorMessage.classList.contains("error")) {
+            exclamationMark.classList.remove("error");            
+            errorMessage.classList.remove("error");            
+        }
+    } else {
+        exclamationMark.classList.add("error");
+        errorMessage.classList.add("error");
+    }
+});
