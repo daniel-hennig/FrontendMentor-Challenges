d02df9142975bc6e05a4fc799baf1e9b9ed80c60
dhnation <danielhennig91@web.de>
2021-10-24 20:58:08 +0100

    upload Fylo Landing Page

---
+++ Fylo Landing Page/README.md
@@ -0,0 +1,47 @@
+# Frontend Mentor - Fylo landing page with two column layout solution
+
+This is a solution to the [Fylo landing page with two column layout challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/fylo-landing-page-with-two-column-layout-5ca5ef041e82137ec91a50f5). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [Preview Solution](https://your-solution-url.com)
+- Live Site URL: [See Live-Version](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS3/ SCSS (Flexbox, Grid, Variables, Mixins)
+- Mobile-first workflow
+- JavaScript
+- Figma Design
+
+## Author
+
+- Website - [Add your name here](https://www.your-site.com)
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
---
+++ Fylo Landing Page/app/script.js
@@ -0,0 +1,56 @@
+'use strict'
+
+/* ========== Form Validation ========== */
+const forms = document.getElementsByTagName('form');
+const email = document.querySelectorAll('.email-input');
+const errorContainer = document.querySelectorAll('.error-container');
+const btnSubmit = document.querySelectorAll('.btn-submit');
+
+for(let i = 0; i < forms.length; i++) { // loop through both forms
+    // input validation
+    forms[i].addEventListener('input', (e) => {
+        if(validateEmail(email[i].value) === false) {
+            if(!errorContainer[i].classList.contains('error')) {
+                errorContainer[i].classList.add('error');
+                email[i].classList.add('--border-red');
+                forms[i].classList.add('--gap-3');
+            }
+        } else {
+            if(errorContainer[i].classList.contains('error')) {
+                errorContainer[i].classList.remove('error');
+                email[i].classList.remove('--border-red');
+                forms[i].classList.remove('--gap-3');
+            }
+        }
+    
+        if(!errorContainer[i].classList.contains('error')) {
+            btnSubmit[i].disabled = false;
+        } else {
+            btnSubmit[i].disabled = true;
+        }
+    });
+    
+    // submit validation
+    forms[0].addEventListener('submit', (e) => {
+        if(email[0].value === '' || email[i].value === null) {
+            if(!errorContainer[i].classList.contains('error')) {
+                errorContainer[i].classList.add('error');
+            }
+        } else {
+            if(errorContainer[i].classList.contains('error')) {
+                errorContainer[i].classList.remove('error');
+            }
+        }
+    
+        if(errorContainer[i].classList.contains('error')) {
+            e.preventDefault();
+        }
+    });
+}
+
+// email-validation regex
+function validateEmail(email) {
+    const regex = /^(([^<>()äüöü\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return regex.test(email);
+}
